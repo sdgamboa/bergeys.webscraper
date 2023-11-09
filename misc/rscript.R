4725e3f9e18b07fab4869d79f6676e924ebd9db3
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-09 18:02:50 -0500

    Add script for fetching abstracts from Bergey's manual (genera only).

--- misc/rscript.R
+++ misc/rscript.R
@@ -1,42 +1,22 @@
-pdfUrl <- "https://onlinelibrary.wiley.com/pb-assets/assets/9781118960608/A_Z_170327-1507044704000.pdf"
-fileName <- tempfile()
-download.file(url = pdfUrl, destfile = fileName)
-txt <- unlist(pdftools::pdf_attachments(fileName))
-
-library(readr)
+library(rvest)
+library(stringr)
 library(purrr)
-txt <- read_file_raw(fileName)
-
-vector_txt <- map_chr(txt, rawToChar)
-vector_txt[[17]]
-
-x <- paste0(vector_txt, collapse = '')
-
-
-grep('http', txt)
-
-
-vector_Txt <- rep("", nb_Txt)
-
-for(i in 1 : nb_Txt) {
-      vector_Txt[i] <- rawToChar(txt[i])
-}
-
-vect
-
-
-r
-
-nb_Txt <- length(txt)
-vector_Txt <- rep("", nb_Txt)
-
-for(i in 1:nb_Txt) {
-      print(i)
-      vector_Txt[i] <- rawToChar(txt[i])
-}
-
-x <- readr::read_file_raw(fileName)
-
-
-
+library(dplyr)
+links <- read.table('misc/genera_abstract_links.txt')[[1]]
+tim <- system.time({
+      abstracts <- map(links[1:10], ~ {
+            html <- rvest::read_html(.x)
+            genus <- rvest::html_node(html, '.citation__title') |>
+                  rvest::html_text2()
+            abstract <- rvest::html_node(html, '.article-section__content p:nth-child(3)' ) |>
+                  rvest::html_text2()
+            data.frame(
+                  genus = genus,
+                  abstract = abstract
+            )
+      }) |>
+            dplyr::bind_rows() |>
+            dplyr::mutate(genus = sub(' *†$', '', genus))
+})
+readr::write_tsv(x = abstracts, file = 'bergeys_abstracts_2023-11-09.tsv')
 

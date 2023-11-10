ba315a0112b26ce024d40fb7c4b2d9c0ee57ad31
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-11-09 19:24:07 -0500

    Update script and add a short example.

--- misc/rscript.R
+++ misc/rscript.R
@@ -2,21 +2,40 @@ library(rvest)
 library(stringr)
 library(purrr)
 library(dplyr)
+library(logr)
+logfile <- "log_file"
+lf <- log_open(logfile, logdir = FALSE, compact = TRUE, show_notes = FALSE)
 links <- read.table('misc/genera_abstract_links.txt')[[1]]
+n <- 1
 tim <- system.time({
-      abstracts <- map(links[1:10], ~ {
-            html <- rvest::read_html(.x)
-            genus <- rvest::html_node(html, '.citation__title') |>
-                  rvest::html_text2()
-            abstract <- rvest::html_node(html, '.article-section__content p:nth-child(3)' ) |>
-                  rvest::html_text2()
+      abstracts <- map(links, ~ {
+            html <- tryCatch(
+                  error = function(e) return(NULL), {
+                        read_html(.x)
+                  }
+            )
+            if (!length(html)) {
+                  n <<- n + 1
+                  message('Index ', n, ' failed.')
+                  return(NULL)
+            }
+            genus <- html_node(html, '.citation__title') |>
+                  html_text2()
+            abstract <- html_node(html, '.article-section__content p:nth-child(3)' ) |>
+                  html_text2()
+            message('Got ', genus, ' - Number ', n, '.')
+            n <<- n + 1
             data.frame(
                   genus = genus,
                   abstract = abstract
             )
       }) |>
-            dplyr::bind_rows() |>
-            dplyr::mutate(genus = sub(' *†$', '', genus))
-})
-readr::write_tsv(x = abstracts, file = 'bergeys_abstracts_2023-11-09.tsv')
-
+            bind_rows() |>
+            mutate(genus = sub(' *†$', '', genus))
+}) |>
+      discard(is.null) |>
+      bind_rows() |>
+      mutate(genus = sub(' *†$', '', genus))
+log_print(tim)
+write_tsv(x = abstracts, file = 'bergeys_abstracts_2023-11-09.tsv')
+log_close()
